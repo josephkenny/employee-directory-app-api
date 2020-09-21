b35fa0bc10e04922e571fb91127605983bcee6fb
josephkenny <39063497+josephkenny@users.noreply.github.com>
2020-09-21 10:54:58 -0400

    Update code spacing and update data source URL for H2 database

--- src/main/java/com/example/demo/EmployeeDirectoryApplication.java
+++ src/main/java/com/example/demo/EmployeeDirectoryApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeDirectoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeDirectoryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeDirectoryApplication.class, args);
+    }
 }
