6c2f0982fff5968b2582b5bdcc709e51b7e9d48b
josephkenny <39063497+josephkenny@users.noreply.github.com>
2020-09-22 07:36:41 -0400

    Add employee entity and validator library

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,72 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Department is required")
+    private String department;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Job title is required")
+    private String jobTitle;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
